3a7efd6719f0f9932979cabff14727902fff232a
andri090500 <firmansyahandri.9500@gmail.com>
2023-03-07 14:18:16 +0700

    code for object detection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import Webcam from "react-webcam";
+import * as tf from "@tensorflow/tfjs";
+import * as cocoModel from "@tensorflow-models/coco-ssd";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [model, setModel] = useState();
+  const [objectName, setObjectName] = useState("");
+  const [objectScore, setObjectScore] = useState("");
+
+  async function loadModel() {
+    try {
+      const dataset = await cocoModel.load();
+      setModel(dataset);
+      console.log("dataset ready...");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    tf.ready().then(() => {
+      loadModel();
+    });
+  }, []);
+
+  async function predict() {
+    const detection = await model.detect(
+      document.getElementById("sourceVideo")
+    );
+    if (detection.length > 0) {
+      detection.map((result, i) => {
+        setObjectName(result.class);
+        setObjectScore(result.score);
+      });
+    }
+  }
+
+  const videoOption = {
+    width: 720,
+    height: 480,
+    facingMode: "environment",
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Object Detection With Coco Model</h1>
+      <div>
+        <button onClick={() => predict()}>Tebak Objek</button>
+      </div>
+      <h3>Objek Deteksi : {objectName ? objectName.toString() : ""}</h3>
+      <h3>Akurasi Deteksi : {objectScore ? objectScore.toString() : ""}</h3>
+      <Webcam id="sourceVideo" audio={false} videoConstraints={videoOption} />
     </div>
   );
 }
